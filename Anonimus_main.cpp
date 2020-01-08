e6453d400b9dfa2612c03c8f5c8ee843209b4087
BojanB1997 <58489629+BojanB1997@users.noreply.github.com>
2020-01-08 12:00:13 +0100

    Dorada dva source fajla.

--- Anonimus_main.cpp
+++ Anonimus_main.cpp
@@ -6,12 +6,44 @@
 
 int main()
 {
-	std::ifstream dat1("Konfiguraciona_datoteka.txt"), dat2("dogadjaji.txt"), dat3("Pitanja.txt"), dat4("Odgovori.txt"),dat5("Kategorije.txt");
-	Anonimus a;
-	a.igrajKviz(dat3, dat4);
-	a.pregledBuducihDogadjaja(dat1, dat2);
-	a.pregledBuducihDogadjaja(dat1, dat2); //primjer visestrukog poziva
-	a.pregledDanasnjihDogadjaja(dat1, dat2);
-	a.pregledProslihDogadjaja(dat1, dat2);
-	a.pregledPoKategoriji(dat2, dat1, dat5);
+	Anonimus korisnik;
+	char opcija;
+	while (true) {
+		std::cout << "=========================================\n";
+		std::cout << "Izaberite zeljenu opciju:\n";
+		std::cout << "=========================================\n";
+		std::cout << "1-Pregled danasnjih dogadjaja\n";
+		std::cout << "2-Pregled buducih dogadjaja\n";
+		std::cout << "3-Pregled proslih dogadjaja\n";
+		std::cout << "4-Pregled dogadjaja po kategoriji\n";
+		std::cout << "5-Dodaj komentar\n";
+		std::cout << "6-Igraj kviz\n";
+		std::cout << "q-Izadji iz programa\n";
+		std::cout << "=========================================\n";
+		std::cin >> opcija;
+		std::cin.ignore();
+		std::ifstream dog, konf, kat, pitanja, odgovori;
+		dog.open("Dogadjaji.txt", std::ios::in);
+		konf.open("Konfiguraciona_datoteka.txt", std::ios::in);
+		kat.open("Kategorije.txt", std::ios::in);
+		pitanja.open("Pitanja.txt", std::ios::in);
+		odgovori.open("Odgovori.txt", std::ios::in);
+		if (opcija == '1')
+			korisnik.pregledDanasnjihDogadjaja(konf, dog);
+		if (opcija == '2')
+			korisnik.pregledBuducihDogadjaja(konf, dog);
+		if (opcija == '3')
+			korisnik.pregledProslihDogadjaja(konf, dog);
+		if (opcija == '4')
+			korisnik.pregledPoKategoriji(dog, konf, kat);
+		if (opcija == '5')
+			//Cekamo da Kljestan zavrsi metodu za dodavanje komentara
+			;
+		if (opcija == '6')
+			korisnik.igrajKviz(pitanja, odgovori);
+		if (opcija == 'q')
+			return 0;
+		std::cout << std::endl;
+	}
+	return 0;
 }
