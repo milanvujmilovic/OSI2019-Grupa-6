cca49464345ff23de467ee8239c823b94021d5c5
Sasa Pejasinovic <sasa.pejasinovic96@gmail.com>
2020-01-08 14:09:30 +0100

    nova funkcionalnost u main-u

--- main.cpp
+++ main.cpp
@@ -1,64 +1,79 @@
 #include <iostream>
 #include <stdlib.h>
-#include "Administrator.h"
-#include "Anonimus.h"
-
+#include "../../osi1/Administrator.h"
 using namespace std;
 
 int main()
 {
     char opcija;
-    Administrator admin;
-			while (true) {
-				std::cout << "=========================================\n";
-				std::cout << "Izaberite  opciju koju zelite: \n";
-				std::cout << "=========================================\n";
-				std::cout << "1-Pregled dogadjaja\n";
-				std::cout << "2-Pregled kategorija\n";
-				std::cout << "3-Uredi dogadjaj\n";
-				std::cout << "4-Dodaj kategoriju\n";
-				std::cout << "5-Obrisi kategoriju\n";
-				std::cout << "6-Ocisti ekran\n";
-				std::cout << "7-Odjava\n";
-				std::cout << "q-Izadji iz programa\n";
-				std::cout << "=========================================\n";
-				std::cin >> opcija;
-				std::cin.ignore();
-				if (opcija == '1')
-				{
-					admin.pregledDogadjaja();
-				}
-				else if (opcija == '2')
-				{
-					admin.pregledKategorija();
-				}
-				else if (opcija == '3')
-				{
-					admin.urediDogadjaj();
-				}
-				else if (opcija == '4')
-				{
-					admin.dodajKategoriju();
-				}
-				else if (opcija == '5')
-				{
-					admin.obrisiKategoriju();
-				}
-				else if (opcija == '6')
-				{
-					system("CLS");
-				}
-				else if (opcija == '7')
-				{
-					admin.odjava();
-				}
-				else if (opcija == 'q')
-				{
-					return 0;
-				}
-				std::cout << endl;
+    Administrator s;
+    while(true){
+    std::cout<<"=========================================\n";
+    std::cout<<"Izaberite  zeljenu opciju: \n";
+    std::cout<<"=========================================\n";
+    std::cout<<"0-Prijava\n";
+    std::cout<<"1-Kreiraj dogadjaj\n";
+    std::cout<<"2-Brisi dogadjaja\n";
+    std::cout<<"3-Pregled dogadjaja\n";
+    std::cout<<"4-Pregled kategorija\n";
+    std::cout<<"5-Uredi dogadjaj\n";
+    std::cout<<"6-Dodaj kategoriju\n";
+    std::cout<<"7-Obrisi kategoriju\n";
+    std::cout<<"8-Ocisti ekran\n";
+    std::cout<<"9-Odjava\n";
+    std::cout<<"q-Izadji iz programa\n";
+    std::cout<<"=========================================\n";
+        std::cin>>opcija;
+        std::cin.ignore();
+         if(opcija=='0')
+        {
+            if(s.provjeraPrijave())
+            s.prijava();
+            else
+            std::cout<<"Vec ste prijavljeni na sistem\n";
+        }
+        else if(opcija=='1')
+        {
+            s.kreirajDogadjaj();
+        }
+        else if(opcija=='2')
+        {
+            s.brisiDogadjaj();
+        }
+         else if(opcija=='3')
+        {
+            s.pregledDogadjaja();
+        }
+        else if(opcija=='4')
+        {
+            s.pregledKategorija();
+        }
+        else if(opcija=='5')
+        {
+           s.urediDogadjaj();
+        }
+        else if(opcija=='6')
+        {
+            s.dodajKategoriju();
+        }
+        else if(opcija=='7')
+        {
+            s.obrisiKategoriju();
+        }
+        else if(opcija=='8')
+        {
+           system("CLS");
+        }
+        else if(opcija=='9')
+        {
+           s.odjava();
+        }
+        else if(opcija=='q')
+        {
+            return 0;
+        }
+        std::cout<<endl;
 
-			}
-	
+    }
     return 0;
 }
