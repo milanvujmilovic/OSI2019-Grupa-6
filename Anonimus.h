d50a2be6bdcf57966bc15e69b5a3536ef7f09d1c
lukak97 <knezic.luka12@gmail.com>
2020-01-09 15:52:37 +0100

    Prosirena verzija Anonimus.h

--- Anonimus.h
+++ Anonimus.h
@@ -1,18 +1,16 @@
 #pragma once
-#include <iostream>
-#include <string>
 #include "Dogadjaj.h"
-#include <fstream>
 #include <stdlib.h>
-#include <ctime>
 #include <sstream>
-#include <vector>
 #include <algorithm>
+
 #pragma warning(disable : 4996)
 
 class Anonimus
 {
 	std::string ime;
+	void citajDogadjaje(std::vector<Dogadjaj>&);
+	std::vector<std::string> split(const std::string& s, char delimiter);
 	std::tm stringToDate(std::string);
 public:
 	Anonimus();
@@ -20,11 +18,10 @@ public:
 	void pregledPoKategoriji(std::ifstream&, std::ifstream&, std::ifstream&);
 	void pregledBuducihDogadjaja(std::ifstream&, std::ifstream&);
 	void pregledProslihDogadjaja(std::ifstream&, std::ifstream&);
-	void dodavanjeKomentara(std::fstream);
+	void dodavanjeKomentara();
 	void igrajKviz(std::ifstream&, std::ifstream&);
 	void pisiDogadjaj(std::vector<std::string>&);
 	int brojDogadjaja(std::ifstream&);
 	tm& konverzijaDatuma();
 	~Anonimus();
 };
-
